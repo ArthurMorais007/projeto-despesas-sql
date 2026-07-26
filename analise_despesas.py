3ecf7d2d4e67addd51cb212e3fba33e8b57220d7
Lan Code <irlLanfevereiro@gmail.com>
2026-07-26 13:05:41 -0300

    Adicionando Média de Gastos

--- analise_despesas.py
+++ analise_despesas.py
@@ -6,3 +6,8 @@ df = pd.read_csv("projeto_despesas.csv")
 
 df['total_gasto'] = df['comida'] + df['academia'] + df['faculdade'] + df['jogo'] + df['assinaturas']
 print(df)
+
+#2 Média de Gastos por mês
+
+media_gastos = df['total_gasto'].mean()
+print(f'\n O valor médio de Gastos é: R${media_gastos:.2f}')
