778abf77e237d4ae497219c11f91ec51cc42e0fb
Lan Code <irlLanfevereiro@gmail.com>
2026-08-01 17:28:37 -0300

    Adiciona estatísticas de gastos e melhora comentários do código

--- analise_despesas.py
+++ analise_despesas.py
@@ -1,13 +1,41 @@
 import pandas as pd 
 
+# ------------------------------------------------------------------------------
+# 1. CARREGAMENTO DOS DADOS
+# ------------------------------------------------------------------------------
+# Importa a base de dados em formato CSV para o DataFrame do Pandas
+
 df = pd.read_csv("projeto_despesas.csv")
 
-#1. Criando Nova Coluna: Total_Gastos
+# ------------------------------------------------------------------------------
+# 2. ADICIONANDO O TOTAL GASTO
+# ------------------------------------------------------------------------------
+# Consolida todas as categorias de despesas individuais em um indicador total
 
 df['total_gasto'] = df['comida'] + df['academia'] + df['faculdade'] + df['jogo'] + df['assinaturas']
 print(df)
 
-#2 Média de Gastos por mês
+# ------------------------------------------------------------------------------
+# 3. MÉDIA DE GASTOS
+# ------------------------------------------------------------------------------
+# Calcula a média dos gastos totais registrados.
 
 media_gastos = df['total_gasto'].mean()
-print(f'\n O valor médio de Gastos é: R${media_gastos:.2f}')
+print(f'\n Gasto Médio: R${media_gastos:.2f}')
+
+
+# ------------------------------------------------------------------------------
+# 4. MAIOR GASTO
+# ------------------------------------------------------------------------------
+# Identifica o maior gasto total entre todos os meses.
+
+maior_gasto_mes = df ['total_gasto'].max()
+print(f'\n O maior gasto registrado foi: R${maior_gasto_mes:.2f}')
+
+# ------------------------------------------------------------------------------
+# 5. MENOR GASTO
+# ------------------------------------------------------------------------------
+# Identifica o menor gasto total entre todos os meses.
+
+menor_gasto_mes = df['total_gasto'].min()
+print(f'\n O menor gasto registrado foi R${menor_gasto_mes:.2f}')
